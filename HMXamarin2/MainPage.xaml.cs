2715f06e76930547c6ca66448941c3bacbb62071
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-11-14 20:30:35 -0600

    Primer Commit

---
+++ HMXamarin2/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HMXamarin2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClicBoton(object sender, EventArgs e)
+        {
+            txtEtiqueta.Text = "Adios"; 
+        }
+    }
+}
